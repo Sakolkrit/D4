1b10aef385ce6d0e285fb93370438020ac0987f3
QuadieZ <62594692+QuadieZ@users.noreply.github.com>
2021-11-28 15:12:52 +0700

    init project files and add normal robot scene

---
+++ project/normal_robot.gd
@@ -0,0 +1,51 @@
+extends Area2D
+
+export var speed = 400
+var screen_size
+var up = false
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	elif Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		#get node
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	# prevent char from leaving the screen
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.y > 0:
+		$AnimatedSprite.animation = "walk_down"
+		$AnimatedSprite.flip_h = false
+		up = false
+	elif velocity.y < 0:
+		$AnimatedSprite.animation = "walk_up"
+		$AnimatedSprite.flip_h = false
+		up = true
+	elif velocity.y == 0:
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = false
+		if up:
+			$AnimatedSprite.animation = "idle_up"
+		else:
+			$AnimatedSprite.animation = "idle_down"
